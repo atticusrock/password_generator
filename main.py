6fccff384301273071a77dde9e9bccd65e6e9e93
atticusrock <atticus.rock@yandex.ru>
2019-08-15 14:31:48 +0300

    add flask request

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import random
 
+from flask import Flask, escape, request
+
 symbols = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789!@#$%^&*'
 
 def generator():
     password = ''.join(random.sample(symbols, len(symbols[0:16])))
     return password
+#print(generator())
+
+app = Flask(__name__)
+
+@app.route('/')
+def password():
+    name = request.args.get("name", "World")
+    return generator()
